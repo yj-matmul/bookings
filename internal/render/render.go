d8bce9904a17ed340a043a12e569ebd68207f016
yj-matmul <yyj3053@gmail.com>
2021-07-11 01:33:45 +0900

    [20210711] finish error response form adding alert

--- internal/render/render.go
+++ internal/render/render.go
@@ -25,6 +25,9 @@ func NewTemplates(a *config.AppConfig) {
 
 // AddDefaultData sets default template data for all templates
 func AddDefaultData(td *models.TemplateData, r *http.Request) *models.TemplateData {
+	td.Flash = app.Session.PopString(r.Context(), "flash")
+	td.Error = app.Session.PopString(r.Context(), "error")
+	td.Warning = app.Session.PopString(r.Context(), "warning")
 	td.CSRFToken = nosurf.Token(r)
 	return td
 }
