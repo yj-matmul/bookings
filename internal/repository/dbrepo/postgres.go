a08656d70146aad1f6475aa321c7a379eac384fa
yj-matmul <yyj3053@gmail.com>
2021-07-27 00:03:20 +0900

    [20210727] fixing bug of room restriction

--- internal/repository/dbrepo/postgres.go
+++ internal/repository/dbrepo/postgres.go
@@ -47,7 +47,7 @@ func (m *postgresDBRepo) InsertRoomRestriction(r models.RoomRestriction) error {
 	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
 	defer cancel()
 
-	stmt := `insert into reservations 
+	stmt := `insert into room_restrictions 
 			 (start_date, end_date, room_id, reservation_id, created_at, updated_at, restriction_id)
 			 values
 			 ($1, $2, $3, $4, $5, $6, $7)`
