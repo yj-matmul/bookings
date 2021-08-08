1a5152afc79278410dc2fb31b40c410e32a89272
yj-matmul <yyj3053@gmail.com>
2021-08-08 15:31:51 +0900

    [20210808] send nicely formatted email

--- cmd/web/send-mail.go
+++ cmd/web/send-mail.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"fmt"
+	"io/ioutil"
 	"log"
+	"strings"
 	"time"
 
 	mail "github.com/xhit/go-simple-mail/v2"
@@ -32,7 +35,18 @@ func sendMsg(m models.MailData) {
 
 	email := mail.NewMSG()
 	email.SetFrom(m.From).AddTo(m.To).SetSubject(m.Subject)
-	email.SetBody(mail.TextHTML, m.Content)
+	if m.Template == "" {
+		email.SetBody(mail.TextHTML, m.Content)
+	} else {
+		data, err := ioutil.ReadFile(fmt.Sprintf("./email-templates/%s", m.Template))
+		if err != nil {
+			app.ErrorLog.Println(err)
+		}
+
+		mailTemplate := string(data)
+		sendToMsg := strings.Replace(mailTemplate, "[%body%]", m.Content, 1)
+		email.SetBody(mail.TextHTML, sendToMsg)
+	}
 
 	err = email.Send(client)
 	if err != nil {
--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -187,10 +187,11 @@ func (m *Repository) PostReservation(w http.ResponseWriter, r *http.Request) {
 		reservation.FirstName, reservation.StartDate.Format("2006-01-02"), reservation.EndDate.Format("2006-01-02"))
 
 	msg := models.MailData{
-		To:      reservation.Email,
-		From:    "me@here.com",
-		Subject: "Reservation Confirmation",
-		Content: htmlMessage,
+		To:       reservation.Email,
+		From:     "me@here.com",
+		Subject:  "Reservation Confirmation",
+		Content:  htmlMessage,
+		Template: "basic.html",
 	}
 	m.App.MailChan <- msg
 
