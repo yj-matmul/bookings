33240d010eea405841772804eed55aa07d018bfe
yj-matmul <yyj3053@gmail.com>
2021-08-09 23:11:53 +0900

    [20210809] create stub handlers for admin function

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -485,3 +485,18 @@ func (m *Repository) Logout(w http.ResponseWriter, r *http.Request) {
 func (m *Repository) AdminDashboard(w http.ResponseWriter, r *http.Request) {
 	render.Template(w, r, "admin-dashboard.page.html", &models.TemplateData{})
 }
+
+//
+func (m *Repository) AdminNewReservations(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-new-reservations.page.html", &models.TemplateData{})
+}
+
+//
+func (m *Repository) AdminAllReservations(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-all-reservations.page.html", &models.TemplateData{})
+}
+
+//
+func (m *Repository) AdminReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	render.Template(w, r, "admin-reservations-calendar.page.html", &models.TemplateData{})
+}
