34db603f94d629f1ce4e56f94a3219a728d4febe
yj-matmul <yyj3053@gmail.com>
2021-08-09 17:32:48 +0900

    [20210809] update other seed field

--- migrations/20210731133116_seed_rooms_table.postgres.up.sql
+++ migrations/20210731133116_seed_rooms_table.postgres.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.rooms (room_name,created_at,updated_at) VALUES
-	 ('General''s Quarters','2021-07-31 00:00:00','2021-07-31 00:00:00'),
-	 ('Major''s Suite','2021-07-31 00:00:00','2021-07-31 00:00:00');
+	 ('General''s Quarters','2021-08-01 00:00:00','2021-08-01 00:00:00'),
+	 ('Major''s Suite','2021-08-01 00:00:00','2021-08-01 00:00:00');
--- migrations/20210731133251_seed_restrictions_table.postgres.up.sql
+++ migrations/20210731133251_seed_restrictions_table.postgres.up.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.restrictions (restriction_name,created_at,updated_at) VALUES
-	 ('Reservation','2021-07-31 00:00:00','2021-07-31 00:00:00'),
-	 ('Owner block','2021-07-31 00:00:00','2021-07-31 00:00:00');
+	 ('Reservation','2021-08-01 00:00:00','2021-08-01 00:00:00'),
+	 ('Owner block','2021-08-01 00:00:00','2021-08-01 00:00:00');
