fa085ed34350c5e1d7b8977aa45fe0074d6eab06
yj-matmul <yyj3053@gmail.com>
2021-08-14 23:15:47 +0900

    [20210814] handle calendar change of post reqeust

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -697,3 +697,8 @@ func (m *Repository) AdminDeleteReservation(w http.ResponseWriter, r *http.Reque
 	m.App.Session.Put(r.Context(), "flash", "Reservation deleted")
 	http.Redirect(w, r, fmt.Sprintf("/admin/reservations-%s", src), http.StatusSeeOther)
 }
+
+// AdminPostReservationsCalendar handles post of reservation calendar
+func (m *Repository) AdminPostReservationsCalendar(w http.ResponseWriter, r *http.Request) {
+	log.Println("works")
+}
