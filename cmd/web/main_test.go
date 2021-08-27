d99fc34ca7eb123da188fbe7358e55be0a493aae
yj-matmul <yyj3053@gmail.com>
2021-08-27 18:34:41 +0900

    [20210827] fix main test go

--- cmd/web/main_test.go
+++ cmd/web/main_test.go
@@ -5,9 +5,9 @@ import (
 )
 
 func TestRun(t *testing.T) {
-	dbInfoPath = "./../../static/db_info.txt"
-	dsn := loadDsn(dbInfoPath)
-	_, err := run(dsn)
+	// dbInfoPath = "./../../static/db_info.txt"
+	// dsn := loadDsn(dbInfoPath)
+	_, err := run()
 	if err != nil {
 		t.Error("fail run()")
 	}
