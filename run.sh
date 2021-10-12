dd810e1c27f92a36c6f3458c0099378eba37ac2b
yj-matmul <yyj3053@gmail.com>
2021-10-12 15:27:06 +0900

    [20211012] work app on background process

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-app/bookings/bookings -dbname=bookings -dbuser=postgres -dbpassword= -production=false -cache=false -logpath=/app/bookings/logs/application_log.txt
+/app/bookings/bookings -dbname=bookings -dbuser=postgres -dbpassword= -production=false -cache=false -logpath=/app/bookings/logs/application_log.txt
--- update.sh
+++ update.sh
@@ -5,3 +5,5 @@ git pull
 soda migrate
 
 go build -o bookings cmd/web/*.go
+
+nohup sh run.sh 1>/dev/null 2>&1 &
